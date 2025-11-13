272c2b942861d246551846a83c8ec13d34f25427
SoulReturns <pascal.hannemann@gmail.com>
2025-11-13 17:45:49 +0100

    Migrate to Kotlin DSL and require MoulConfig.

---
+++ build.gradle.kts
@@ -0,0 +1,99 @@
+plugins {
+	id("fabric-loom") version "1.13-SNAPSHOT"
+    `maven-publish`
+	id("org.jetbrains.kotlin.jvm") version "2.2.21"
+    id("com.gradleup.shadow") version "8.3.5"
+}
+
+version = project.property("mod_version") as String
+group = project.property("maven_group") as String
+
+base {
+	archivesName = project.property("archives_base_name") as String
+}
+
+repositories {
+	// Add repositories to retrieve artifacts from in here.
+	// You should only use this when depending on other mods because
+	// Loom adds the essential maven repositories to download Minecraft and libraries from automatically.
+	// See https://docs.gradle.org/current/userguide/declaring_repositories.html
+	// for more information about repositories.
+
+    maven("https://maven.notenoughupdates.org/releases/")
+}
+
+val shadowModImpl by configurations.creating {
+    configurations.modImplementation.get().extendsFrom(this)
+}
+
+dependencies {
+	// To change the versions see the gradle.properties file
+	minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
+	mappings("net.fabricmc:yarn:${project.property("yarn_mappings")}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
+
+	// Fabric API. This is technically optional, but you probably want it anyway.
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+	modImplementation("net.fabricmc:fabric-language-kotlin:${project.property("fabric_kotlin_version")}")
+
+    // MoulConfig
+    "shadowModImpl"("org.notenoughupdates.moulconfig:modern-1.21.7:4.2.0-beta")
+}
+
+tasks.shadowJar {
+    // Make sure to relocate MoulConfig to avoid version clashes with other mods
+    configurations = listOf(shadowModImpl)
+    relocate("io.github.notenoughupdates.moulconfig", "my.mod.deps.moulconfig")
+}
+
+tasks.processResources {
+	inputs.property("version", project.version)
+
+	filesMatching("fabric.mod.json") {
+		expand(mapOf("version" to project.version))
+	}
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+	options.release = 21
+}
+
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
+	compilerOptions {
+		jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+	}
+}
+
+java {
+	// Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+	// if it is present.
+	// If you remove this line, sources will not be generated.
+	withSourcesJar()
+
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.jar {
+	from("LICENSE") {
+		rename { "${it}_${base.archivesName.get()}"}
+	}
+}
+
+// configure the maven publication
+publishing {
+	publications {
+		create<MavenPublication>("mavenJava") {
+			artifactId = project.property("archives_base_name") as String
+			from(components["java"])
+		}
+	}
+
+	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+	repositories {
+		// Add repositories to publish to here.
+		// Notice: This block does NOT have the same function as the block in the top level.
+		// The repositories here will be used for publishing your artifact, not for
+		// retrieving dependencies.
+	}
+}
